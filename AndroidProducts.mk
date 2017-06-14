dfb98179090b26c2ea3dfcff1687aef54280c9a1
Lukas Berger <mail@lukasberger.at>
2017-06-14 22:36:12 +0200

    zero: create unified device-tree for Lineage/AOKP
    Change-Id: I614b0baa87f0dacfee123ecee31fa8f591ba0390

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,10 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_zerofltexx.mk \
+    $(LOCAL_DIR)/full_zerofltexx.mk
+
+ifneq ($(AOKP_VERSION),)
+PRODUCT_MAKEFILES += \
+    $(LOCAL_DIR)/aokp.mk
+else
+PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/lineage.mk
+endif
---
+++ aokp.mk
@@ -0,0 +1,18 @@
+# Initialise device config
+$(call inherit-product, device/samsung/zerofltexx/full_zerofltexx.mk)
+
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Enhanced NFC
+# $(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Galaxy S6" \
+    TARGET_DEVICE="Galaxy S6"
+
+PRODUCT_NAME := aokp_zerofltexx
+PRODUCT_DEVICE := zerofltexx
