ce7d93a0e5f2e2a3709d302d6d72ae7e7fd93885
Lukas0610 <mail@lukasberger.at>
2017-08-22 17:28:41 +0200

    zero: use new Lineage 15.0 vendor-repo

--- aosp_zerofltexx.mk
+++ aosp_zerofltexx.mk
@@ -5,10 +5,10 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Enhanced NFC
-# $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# $(call inherit-product, vendor/lineage/config/nfc_enhanced.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit properties for TeamNexus-bases ROMs
 $(call inherit-product, vendor/nexus/product.mk)
