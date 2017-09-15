e14f8a8ecefea55a5ea3cc84a10d0467e3532daa
Lukas0610 <mail@lukasberger.at>
2017-09-15 09:09:02 +0200

    Revert "zero: enabled experimental dual-SIM support"
    This reverts commit dd3c38b503ca8374c04df491d534cac21dfb353d.

--- device.mk
+++ device.mk
@@ -19,8 +19,5 @@ LOCAL_PATH := device/samsung/zerofltexx
 ## device overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-# Dual-SIM Support
-BOARD_HAS_DUAL_SIM := true
-
 # Inherit from zero-common
 $(call inherit-product, device/samsung/zero-common/zero-common.mk)
