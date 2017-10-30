90afa1407e5a61d9c180e35902ab12e7defe6ffc
Lukas0610 <mail@lukasberger.at>
2017-10-30 23:04:28 +0100

    zerofltexx: nexus-vendor is now included by build-system
    Change-Id: I6b39ff3b94207f51e6dfd51f3b569afa30873ecc

--- lineage.mk
+++ lineage.mk
@@ -10,9 +10,6 @@ TARGET_SCREEN_WIDTH := 1440
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit properties for TeamNexus-bases ROMs
-$(call inherit-product, vendor/nexus/product.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerofltexx" \
     TARGET_DEVICE="zeroflte"
--- omni_zerofltexx.mk
+++ omni_zerofltexx.mk
@@ -10,9 +10,6 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 # Inherit from our omni product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit properties for TeamNexus-bases ROMs
-$(call inherit-product, vendor/nexus/product.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="zerofltexx" \
     TARGET_DEVICE="zeroflte"
