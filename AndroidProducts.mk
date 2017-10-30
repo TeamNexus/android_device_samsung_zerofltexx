ba9781da5f5c5b4dad608b9ef3939627731f46a7
Lukas0610 <mail@lukasberger.at>
2017-10-30 17:45:45 +0100

    zerofltexx: added product-makefile for GZOSP
    Change-Id: Ib79da9f3ac5ddd61de13ee1c4e2f9b543c7257fe

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,5 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zerofltexx.mk \
     $(LOCAL_DIR)/aokp.mk \
     $(LOCAL_DIR)/aosp_zerofltexx.mk \
+    $(LOCAL_DIR)/gzosp.mk \
     $(LOCAL_DIR)/lineage.mk \
     $(LOCAL_DIR)/omni_zerofltexx.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,5 +16,6 @@
 
 add_lunch_combo aokp_zerofltexx-userdebug
 add_lunch_combo aosp_zerofltexx-userdebug
+add_lunch_combo gzosp_zerofltexx-userdebug
 add_lunch_combo lineage_zerofltexx-userdebug
 add_lunch_combo omni_zerofltexx-userdebug
