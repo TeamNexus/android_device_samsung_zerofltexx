2f8ce8cd61b4a6cc4dd0ad8e8ab001919daf963e
Lukas0610 <mail@lukasberger.at>
2018-01-26 22:23:46 +0100

    zerofltexx: updated asserts to support Edge-variants
    Change-Id: Ie7ad8acf922fe1f06b9349db2908d5db62809ad6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 include device/samsung/zero-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := zerofltexx,zeroflte
+TARGET_OTA_ASSERT_DEVICE := zerofltexx,zeroflte,zeroltexx,zerolte
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zerofltexx/include
