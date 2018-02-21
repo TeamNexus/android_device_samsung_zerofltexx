239ffacdd909fe00d13006b06bbfcfa2e7306faf
Lukas0610 <mail@lukasberger.at>
2018-02-21 11:15:58 +0100

    zerofltexx: move size of system-image to common
    Change-Id: Ia7acea704aed7a529b4535769967d8e3e6ef858c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,8 +32,5 @@ TARGET_SPECIFIC_HEADER_PATH += device/samsung/zerofltexx/include
 # Kernel
 TARGET_KERNEL_CONFIG := lineageos_zerofltexx_defconfig
 
-# Partitions
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3879731200
-
 # Radio
 BOARD_MODEM_TYPE := ss333
