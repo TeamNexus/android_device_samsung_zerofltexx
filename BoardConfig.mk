3a5c524ccd7d0b584e60b46ef75f2587a367ded8
Lukas0610 <mail@lukasberger.at>
2018-02-24 01:10:58 +0100

    zerofltexx: now supports S/K/L!
    Change-Id: Ibd5f75c4765018b0a55e3b4942c5198222ce829b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 include device/samsung/zero-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := zerofltexx,zeroflte,zeroltexx,zerolte
+TARGET_OTA_ASSERT_DEVICE := zerofltexx,zeroflte,zeroflteskt,zeroltexx,zerolte,zerolteskt
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zerofltexx/include
