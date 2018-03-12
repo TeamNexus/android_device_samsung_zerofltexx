952ece7d8984c91808861b6d5b51339cbf7a51a7
Lukas0610 <mail@lukasberger.at>
2018-03-12 15:11:50 +0100

    zerofltexx: add asserts for T/W8
    Change-Id: Iab1c34b2854c7b4b26b22ca4b2ddff072a557430

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,7 @@
 include device/samsung/zero-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := zerofltexx,zeroflte,zeroflteskt,zeroltexx,zerolte,zerolteskt
+TARGET_OTA_ASSERT_DEVICE := zerofltexx,zeroflte,zeroflteskt,zerofltecan,zerofltetmo,zeroltexx,zerolte,zerolteskt,zeroltecan,zeroltetmo
 
 # Include path
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/zerofltexx/include
