d0e35a7dc478111e2c01d2fe8e528304ff4098e8
Lukas0610 <mail@lukasberger.at>
2018-09-07 21:33:30 +0200

    zerofltexx: rename lineage product makefile to support Pie

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -3,6 +3,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aokp.mk \
     $(LOCAL_DIR)/aosp_zerofltexx.mk \
     $(LOCAL_DIR)/gzosp.mk \
-    $(LOCAL_DIR)/lineage.mk \
+    $(LOCAL_DIR)/lineage_zerofltexx.mk \
     $(LOCAL_DIR)/nexus_zerofltexx.mk \
     $(LOCAL_DIR)/omni_zerofltexx.mk
